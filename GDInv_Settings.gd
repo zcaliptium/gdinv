fa588dc895c5657c0eac05a8967d7e4f52b87d5b
zcaliptium <zcaliptium@gmail.com>
2020-02-07 14:58:26 +0300

    Some tweaks for the settings system.

--- GDInv_Settings.gd
+++ GDInv_Settings.gd
@@ -1,30 +1,35 @@
 # Copyright (c) 2019-2020 ZCaliptium.
 extends Object
 
-const PROP_PATHS = "PluginSettings/gdinv/ItemJsonPaths";
-const PROP_LOADONREADY = "PluginSettings/gdinv/LoadOnReady";
+const PREFIX = "PluginSettings/gdinv/"
+
+const PROP_PATHS = PREFIX + "ItemJsonPaths";
+const PROP_LOADONREADY = PREFIX + "LoadOnReady";
 
 const PROPERTIES: Array = [
 	{
-	    "name": PROP_PATHS,
-	    "type": TYPE_STRING_ARRAY,
-	    "hint": PROPERTY_HINT_DIR
+		"name": PROP_PATHS,
+		"type": TYPE_STRING_ARRAY,
+		"hint": PROPERTY_HINT_DIR
 	},
 	{
-	    "name": PROP_LOADONREADY,
-	    "type": TYPE_BOOL
+		"name": PROP_LOADONREADY,
+		"type": TYPE_BOOL
 	}
 ];
 
+const DEFAULTS: Dictionary = {
+	PROP_PATHS: {},
+	PROP_LOADONREADY: true
+}
+
 # Loads settings related to this plugin.
 static func load_settings() -> void:
-	set_default(PROP_PATHS, {});
-	set_default(PROP_LOADONREADY, true);
-
 	for i in range(0, PROPERTIES.size()):
 		var property_info: Dictionary = PROPERTIES[i];
+		set_default(property_info["name"], DEFAULTS.get(property_info["name"]));
 		ProjectSettings.add_property_info(property_info);
-	
+
 static func get_option(name: String):
 	return ProjectSettings.get(name);
 
